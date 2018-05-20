7ad8d4563d59559f6c40a842288822d0251225fa
Alexander Poole <alex.o.poole@gmail.com>
2018-05-20 18:52:41 +0200

    Implemented non functioning backpropogation§

--- backpropogation.py
+++ backpropogation.py
@@ -8,7 +8,7 @@ def sigmoid(x):
 
 def sigmoid_delta(x):
     fx = sigmoid(x)
-    return fx * (1 - fx)
+    return np.multiply(fx, (1 - fx))
 
 
 def cost_function(labels, output):
@@ -19,31 +19,49 @@ def weighted_input_function(weights, input_, bias):
     return np.matmul(weights, input_) + bias
 
 
-def neuron(weights, input_, bias):
-    return sigmoid(weighted_input_function(weights, input_, bias))
-
-
 def create_weights(l, k):
-    return np.random.rand(l, k)
+    return np.matrix(np.random.rand(l, k))
 
 
 def create_bias(l):
-    return np.random.rand(l)
+    return np.transpose(np.matrix(np.random.rand(l)))
+
+
+def bp1(x, y, z):
+    a = np.subtract(x, y)
+    return np.multiply(a, sigmoid_delta(z))
+
+
+def bp2(w, error, z):
+    return np.multiply(np.matmul(np.transpose(w), error), z)
 
 
 class NeuronLayer:
     def __init__(self, inputs_, neurons):
         self.weights = create_weights(neurons, inputs_)
         self.bias = create_bias(neurons)
+        self.step = 0.1
 
-    def update_weights(self, w):
-        self.weights += w
+    def update_layer(self):
+        self.delta_w = np.multiply(self.step, np.transpose(np.matmul(self.a_in, np.transpose(self.error))))
+        self.weights = self.weights - self.delta_w
 
-    def update_bias(self, b):
-        self.bias += b
+        self.delta_b = np.multiply(self.step, self.error)
+        self.bias = self.bias - self.delta_b
 
     def input(self, input_):
-        return neuron(self.weights, input_, self.bias)
+        self.z = weighted_input_function(self.weights, input_, self.bias)
+        self.a_in = input_
+        self.a_out = sigmoid(self.z)
+        return self.a_out
+
+    def error1(self, x, y):
+        self.error = bp1(x, y, self.z)
+        return self.error
+
+    def error2(self, weights, previous_error):
+        self.error = bp2(weights, previous_error, self.z)
+        return self.error
 
 
 def generate_sample():
@@ -53,16 +71,15 @@ def generate_sample():
            [5, 10, 0],
            [0, 0, 10]]
 
-    x1 = np.random.multivariate_normal(mean1, cov)
-    x2 = np.random.multivariate_normal(mean2, cov)
+    x1 = np.transpose(np.matrix(np.random.multivariate_normal(mean1, cov)))
+    x2 = np.transpose(np.matrix(np.random.multivariate_normal(mean2, cov)))
 
     if random.choice([True, False]):
         x = x1
-        y = (1, 0)
+        y = np.transpose(np.matrix([1, 0]))
     else:
         x = x2
-        y = (0, 1)
-
+        y = np.transpose(np.matrix([0, 1]))
     return x, y
 
 
@@ -71,14 +88,28 @@ if __name__ == '__main__':
             NeuronLayer(3, 4),
             NeuronLayer(4, 2)]
 
-
-
     i = 0
-    while i < 1000:
+    while i < 100000:
         i += 1
 
         x, y = generate_sample()
+        sample = x
+        x = np.multiply(1 / 255.0, x)
         for layer in myNN:
             x = layer.input(x)
 
-        
+        print("\n")
+        print("* result")
+        print(x)
+        print("* label")
+        print(y)
+        print("* sample")
+        print(sample)
+
+
+        myNN[2].error1(x, y)
+        myNN[1].error2(myNN[2].weights, myNN[2].error)
+        myNN[0].error2(myNN[1].weights, myNN[1].error)
+
+        for layer in myNN:
+            layer.update_layer()
--- test.py
+++ test.py
@@ -4,8 +4,8 @@ import numpy as np
 
 # Testing my sigmoids
 def test_sigmoid_0():
-    zeros = np.array([0, 0, 0])
-    half = np.array([0.5, 0.5, 0.5])
+    zeros = np.matrix([0, 0, 0])
+    half = np.matrix([0.5, 0.5, 0.5])
     np.testing.assert_array_equal(sigmoid(zeros), half)
 
 
@@ -19,33 +19,38 @@ def test_sigmoid_negative_inf():
 
 # Testing my cost
 def test_cost_should_be_zero():
-    test_vec = np.array([1, 2, 3])
+    test_vec = np.matrix([1, 2, 3])
     assert cost_function(test_vec, test_vec) == 0
 
 
 def test_cost_should_be_zero():
-    labels = np.array([1, 1, 1])
-    output = np.array([0, 0, 0])
+    labels = np.matrix([1, 1, 1])
+    output = np.matrix([0, 0, 0])
     assert cost_function(labels, output) == 3 / 2
+
 
 def test_weighted_input():
     weights = np.eye(2, dtype=int)
-    input_ = np.array([1, 0])
-    bias = np.array([1, 2])
-    result = np.array([2, 2])
+    input_ = np.matrix([1, 0])
+    bias = np.matrix([1, 2])
+    result = np.matrix([2, 2])
 
     np.testing.assert_array_equal(weighted_input_function(weights, input_, bias), result)
 
+
 def test_neuron_layer():
     weights = np.eye(2, dtype=int)
-    input_ = np.array([1, 0])
-    bias = np.array([-1, 0])
-    result = np.array([0.5, 0.5])
+    input_ = np.matrix([1, 0])
+    bias = np.matrix([-1, 0])
+    result = np.matrix([0.5, 0.5])
 
-    np.testing.assert_array_equal(neuron(weights, input_, bias), result)
+    np.testing.assert_array_equal(sigmoid(weighted_input_function(weights, input_, bias), result))
 
 
-def test_neuron_layer():
-    myNN = NeuronLayer(2, 4)
-    output = myNN.input(np.array([1, 2]))
-    print(output)
+def test_BP1_correct_output():
+    x = np.matrix([1, 0])
+    y = np.matrix([1, 0])
+    z = np.matrix([1, 1])
+    result = np.matrix([0, 0])
+
+    np.testing.assert_array_equal(bp1(x, y, z), result)
